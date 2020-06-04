9b3a4b9a75b6351bc5ccb40eada2731e67df7078
JuanCalMun <juancalmun@gmail.com>
2020-06-04 21:35:56 +0200

    Deleted the port information in Dto, now it ll be logged in each service

--- items/src/main/java/com/springboot/app/items/SpringbootServiceItemApplication.java
+++ items/src/main/java/com/springboot/app/items/SpringbootServiceItemApplication.java
@@ -2,12 +2,13 @@ package com.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "products-service")
-@EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class SpringbootServiceItemApplication {
 
     public static void main(final String[] args) {
